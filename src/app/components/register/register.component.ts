64a85b8d07ac50173925b3a32b4be75d949fcc72
Henrique <henrique.krastev@rewe-digital.com>
2023-07-14 14:07:27 +0300

    created register component

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import {AuthenticationService} from "../../services/authentication.service";
+import {Router} from "@angular/router";
+import {FormGroup, NonNullableFormBuilder, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+
+  form: FormGroup = this.fb.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    email: ['', [Validators.required, Validators.email]]
+  })
+
+  constructor(private authService: AuthenticationService,
+              private router: Router,
+              private fb: NonNullableFormBuilder) {
+  }
+
+  register() {
+
+    const username = this.form.get('username')?.value;
+    const password =  this.form.get('password')?.value;
+
+    this.authService
+      .register(username, password)
+      .subscribe(registerSuccessful => {
+
+        if (registerSuccessful) {
+          this.router.navigate(['login', {username: username}])
+        } else {
+          console.log('user already exists')
+        }
+
+
+      })
+
+  }
+
+}
