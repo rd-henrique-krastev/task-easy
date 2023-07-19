1b24a763e82f9c8999555d0d938f8bdc43163c63
Kaloyan Marinov <kaloyan.marinov@rewe-digital.com>
2023-07-20 01:18:11 +0300

    change dashboard styling to scss

--- src/app/card.ts
+++
@@ -1,7 +0,0 @@
-export interface Card {
-    cardName: string;
-    cardStatus: string;
-    cardAsignee: string;
-    description: string;
-}
-
---
+++ src/app/models/task.model.ts
@@ -0,0 +1,7 @@
+export class Task {
+  constructor(
+   public title: string,
+   public asignee: string,
+   public description: string,
+  ) {}
+}
